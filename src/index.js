a48803c00860ee51bbf687ae1314f7e268a54396
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-25 09:17:27 -0400

    add react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-document.body.innerHTML = "<h1>Hello World</h1>";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    )
+}
+
+const $root = document.querySelector('.js-root');
+ReactDOM.render(<Counter />, $root);
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,25 @@ module.exports = {
     devServer: {
         contentBase: "./dist"
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "Development"
+            title: "Development",
+            template: "./src/index.html"
         })
     ],
     output: {
