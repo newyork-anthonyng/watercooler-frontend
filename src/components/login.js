b3bf5c43bad8b9fc66bc9a5a0eba97d9aa25103c
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-25 16:13:37 -0400

    add login component (#1)

---
+++ src/components/login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useMachine } from "@xstate/react";
+import { loginStateMachine, initMachineOptions } from "./loginStateMachine";
+
+const LogInForm = () => {
+  const [state, send] = useMachine(loginStateMachine, initMachineOptions);
+
+  const handleEmailChange = (e) => {
+    send({ type: "INPUT_EMAIL", email: e.target.value });
+  };
+  const handlePasswordChange = (e) => {
+    send({ type: "INPUT_PASSWORD", password: e.target.value });
+  };
+  const handleSubmit = () => {
+    send({ type: "SUBMIT" });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Email:
+          <input
+            type="text"
+            value={state.context.email}
+            onChange={handleEmailChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={state.context.password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+      </div>
+
+      <button
+        type="submit"
+        disabled={state.matches("waitingResponse")}
+        onClick={handleSubmit}
+      >
+        Login
+      </button>
+
+      {state.matches("waitingResponse") && <p>Signing in...</p>}
+      {state.matches("ready.authService.error.communication") &&
+        "Something happened with the network call. Try again."}
+      {state.matches("ready.authService.error.authentication") &&
+        "Email/password combination do not match."}
+      {state.matches("ready.email.error.empty") && "Email is blank."}
+      {state.matches("ready.password.error.empty") && "Password is blank."}
+    </form>
+  );
+};
+
+export default LogInForm;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import LogInForm from "./components/login";
 
 function Counter() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <LogInForm />
     </div>
   );
 }
