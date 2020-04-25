4bee5a8f01a8ad8cfbccb860e02d00b0410971cd
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-25 09:21:33 -0400

    add prettier

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 function Counter() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>+</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 }
 
-const $root = document.querySelector('.js-root');
+const $root = document.querySelector(".js-root");
 ReactDOM.render(<Counter />, $root);
--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        app: "./src/index.js"
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase: "./dist"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: "Development",
-            template: "./src/index.html"
-        })
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
     ],
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
-    }
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
